58b8f63f08163593567543e268f3b5ab68626228
CAREER_SH\raven.jiang <raven.jiang@51job.com>
2019-11-20 17:31:43 +0800

    @SCROPE 设置组件作用域

---
+++ src/main/java/com/xinyi/config/MyConfig1.java
@@ -0,0 +1,19 @@
+package com.xinyi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import com.xinyi.bean.Person;
+
+@Configuration
+public class MyConfig1 {
+	
+	@Scope("prototype")
+	//默认单实例
+	@Bean("person")
+	public Person person() {
+		System.out.println("IOC容器中注入person实例");
+		return new Person("李青",18);
+	}
+}
--- src/main/java/com/xinyi/test/IOCTest.java
+++ src/main/java/com/xinyi/test/IOCTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.xinyi.config.MyConfig;
+import com.xinyi.config.MyConfig1;
 
 public class IOCTest {
 	@Test
@@ -13,5 +14,19 @@ public class IOCTest {
 		for(String string : strings) {
 			System.out.println(string);
 		}
+		
+	}
+	
+	@Test
+	public void test2() {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MyConfig1.class);
+//		String[] strings = applicationContext.getBeanDefinitionNames();
+//		for(String string : strings) {
+//			System.out.println(string);
+//		}
+		//默认单实例
+		Object person1 = applicationContext.getBean("person");
+		Object person2 = applicationContext.getBean("person");
+		System.out.println(person1 == person2);
 	}
 }
